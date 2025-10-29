489f722dbe7a74b98f0244710674ab6d3f140318
Samira Alvarez <132599983+Samira-AA@users.noreply.github.com>
2025-10-29 18:24:38 -0500

    chore: add folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import agropapinLogo from './assets/agropapin.png'
+import fichajeLogo from '/fichaje.jpeg'
 import './App.css'
 
 function App() {
@@ -10,10 +10,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={fichajeLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={agropapinLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
