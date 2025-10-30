08bf02506d6ec0ebf5002430abfe535b0e250639
Samira Alvarez <132599983+Samira-AA@users.noreply.github.com>
2025-10-29 23:13:22 -0500

    feat: add navbar and routes config

---
+++ src/shared/components/layouts/AuthLayout.tsx
@@ -0,0 +1,17 @@
+import {Outlet} from "react-router-dom";
+import {Navbar} from "../ui/Navbar.tsx";
+
+export default function AuthLayout(){
+    return(
+        <div className="main-layout">
+            <header>
+                <Navbar/>
+            </header>
+
+            <main>
+                <Outlet></Outlet>
+            </main>
+
+        </div>
+    )
+}
